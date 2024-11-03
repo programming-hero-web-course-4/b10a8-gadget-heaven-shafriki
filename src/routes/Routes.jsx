0cf90641885d9b2dbf66cad949c053531d2a6cf2
shafriki <shafriki.cse@gmail.com>
2024-11-03 19:27:40 +0600

    Bismillah, Initial Setup for Assignment 08 Gadget Heaven

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,29 @@
+import App from "../App";
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from "../pages/Home";
+import Statistics from "../pages/Statistics";
+import Dashboard from "../pages/Dashboard";
+import MainLayout from "../layouts/MainLayout";
+
+const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+            path: '/home',
+            element: <Home></Home>
+        },
+        {
+            path: '/statistics',
+            element: <Statistics></Statistics>
+        },
+        {
+            path: '/dashbord',
+            element: <Dashboard></Dashboard>
+        }
+      ]
+    },
+  ]);
+
+  export default routes;
