7ce993c2fb66d26fbaef14492ace27f10cf67738
shafriki <shafriki.cse@gmail.com>
2024-11-05 06:46:39 +0600

    branch design changed

--- src/components/Branch.jsx
+++ src/components/Branch.jsx
@@ -9,9 +9,15 @@ const Branch = () => {
     const branchData = useLoaderData();
 
     return (
-        <div className="grid grid-cols-1 px-3 md:px-0 md:grid-cols-4 gap-5 max-w-screen-xl mx-auto mt-10">
+        <div className="grid grid-cols-1 px-3 md:px-0 md:grid-cols-3 gap-5 max-w-screen-xl mx-auto mt-10">
             {branchData.map((branch) => (
-                <div key={branch.contact} className="p-4 bg-green-100 border rounded shadow-md">
+                <div key={branch.contact} className="p-4 bg-[#D6F1F9] border-2 border-[#3B6978] rounded-lg shadow-md flex items-center gap-5">
+
+                    {/* img */}
+                    <div><img src="GadgetHeaven.png" alt="" className='w-24 border-2 border-[#3B6978] rounded-lg' /></div>
+                    
+                    {/* text */}
+                    <div>
                     <h2 className="font-semibold text-lg">{branch.branchName}</h2>
                     <p className='text-gray-500 text-sm'>
                         <i className="fa-solid fa-location-dot mr-1 text-blue-500"></i>
@@ -21,6 +27,7 @@ const Branch = () => {
                         <i className="fa-solid fa-address-book mr-1 text-green-500"></i>
                         Contact: {branch.contact}
                     </p>
+                    </div>
                 </div>
             ))}
         </div>
